e067116f2909bee9e4483935709f997d5087673b
Prabhat Ranjan <prabhat.hcu@gmail.com>
2026-06-29 09:36:42 +0000

    fixed the dark light theme issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // ← Ye line hona chahiye!
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
